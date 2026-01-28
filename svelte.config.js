80d5b6d9eec2110fee43f996473711aeab093152
Gentman Tan <gentmantan@gmail.com>
2026-01-28 15:07:06 -0500

    Enable svelte build and remote functions

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,15 @@
-import adapter from '@sveltejs/adapter-node';
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
+import adapter from "svelte-adapter-bun";
 
 /** @type {import('@sveltejs/kit').Config} */
-const config = { kit: { adapter: adapter() } };
+const config = {
+  preprocess: vitePreprocess(),
+  kit: { adapter: adapter(), experimental: { remoteFunctions: true } },
+  compilerOptions: {
+    experimental: {
+      async: true,
+    },
+  },
+};
 
 export default config;
