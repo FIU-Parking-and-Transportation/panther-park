f4318e8811ad8b84678a83b35c4d356555b74c58
Gentman Tan <gentmantan@gmail.com>
2026-03-18 09:53:32 -0400

    Add performance logging hook

---
+++ src/hooks.server.ts
@@ -0,0 +1,18 @@
+import { LOG_LEVEL } from "$env/static/private";
+import type { Handle } from "@sveltejs/kit";
+
+export const handle: Handle = async ({ event, resolve }) => {
+  const start = LOG_LEVEL == "verbose" ? performance.now() : null;
+
+  const response = await resolve(event);
+  if (start != null) {
+    console.log(
+      "Request to",
+      event.url.pathname,
+      "processed in",
+      performance.now() - start,
+      "ms",
+    );
+  }
+  return response;
+};
