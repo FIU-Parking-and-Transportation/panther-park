f956f5eb2e5ae615d4c280dc0173e0d0e74e1f95
Gentman Tan <gentmantan@gmail.com>
2026-05-26 14:42:13 -0400

    Fix log flag logic

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -2,7 +2,7 @@ import { LOG_LEVEL } from "$env/static/private";
 import type { Handle } from "@sveltejs/kit";
 
 export const handle: Handle = async ({ event, resolve }) => {
-  const start = LOG_LEVEL == "verbose" || "debug" ? performance.now() : null;
+  const start = (LOG_LEVEL === "verbose" || LOG_LEVEL === "debug") ? performance.now() : null;
 
   const response = await resolve(event);
   if (start != null) {
